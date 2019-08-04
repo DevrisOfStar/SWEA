57dcae9b65dce34346a6e1e9428cf3d0f0b9a6fc
Nokda <yh1483@naver.com>
2019-08-04 23:40:03 +0900

    2019-08-04 Problem_1234 Solved

--- src/Problem_1234/Solution.java
+++ src/Problem_1234/Solution.java
@@ -3,7 +3,6 @@ package Problem_1234;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {

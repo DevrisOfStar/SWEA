47521afc7394a11b0f46fd7424844241d626b56c
nokda75 <yh1483@naver.com>
2019-08-05 08:57:26 +0900

    2019-08-05 8104 Solved.

--- src/Problem_8104/Solution.java
+++ src/Problem_8104/Solution.java
@@ -3,34 +3,38 @@ package Problem_8104;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-/* 메모리 오류가 뜸 : 조정 필요 */
-public class Solution {
 
-	public static void solution(long n, long k) {
-		long num = 0;
-		if (n % 2 == 0) {
-			num = n / 2;
-			num = 2 * k * num * num + num;
-		} else {
-			num = (n + 1) / 2;
-			num = 2 * k * num * (num - 1) + num;
-		}
-		
-		
-		for(int i = 0; i < k; i++)
-			System.out.print(num + " ");
-		System.out.println();
-	}
+/* 완료 */
+public class Solution {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine());
-
+		String[] str;
 		for (int t = 1; t <= T; t++) {
-			String[] str = br.readLine().split(" ");
+			 str = br.readLine().trim().split(" ");
+			long n = Long.parseLong(str[0]);
+			long k = Long.parseLong(str[1]);
 
-			solution(Long.parseLong(str[0]), Long.parseLong(str[1]));
+			long answer = 0;
+			long num = 0;
+			num = (n + 1) / 2;
+			answer = 2 * k * num;
+			if (n % 2 == 0) {
+				answer = answer * num + num;
+			} else {
+				answer = answer * (num - 1) + num;
+			}
+
+			int cnt = 0;			
+			System.out.printf("#%d ",t);
+			for (int i = 0; i < k; i++) {
+				System.out.print((answer + cnt) + " ");
+				if(n%2 == 1) cnt++;
+			}
+
+			System.out.println();
 		}
 	}
 }

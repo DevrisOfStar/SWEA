fb02b10b9b9e85dcb925b3d332a0120bc767409a
Nokda <yh1483@naver.com>
2019-08-14 23:20:57 +0900

    2019-08-14 Problem 1230 Solved.

---
+++ src/Problem_1230/Solution.java
@@ -0,0 +1,59 @@
+package Problem_1230;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int t = 1; t <= 10; t++) {
+			int k = Integer.parseInt(br.readLine());
+			List<String> arr = new ArrayList<>();
+			String[] s = br.readLine().trim().split(" ");
+			for (int i = 0; i < s.length; i++) {
+				arr.add(s[i]);
+			}
+			int n = Integer.parseInt(br.readLine());
+			String[] op = br.readLine().trim().split(" ");
+			int cnt = 0;
+			for (int i = 0; i < n; i++) {
+				String option = op[cnt++];
+				if (option.equals("I")) {
+					int start = Integer.parseInt(op[cnt++]);
+					int count = Integer.parseInt(op[cnt++]);
+
+					String[] temp = new String[count];
+					for (int j = temp.length - 1; j >= 0; j--) {
+						temp[j] = op[cnt++];
+					}
+
+					for (int j = 0; j < temp.length; j++) {
+						arr.add(start, temp[j]);
+					}
+
+				} else if (option.equals("D")) {
+					int start = Integer.parseInt(op[cnt++]);
+					int count = Integer.parseInt(op[cnt++]);
+					for (int j = 0; j < count; j++)
+						arr.remove(start);
+				} else if (option.equals("A")) {
+					int count = Integer.parseInt(op[cnt++]);
+					
+					for (int j = 0; j < count; j++) {
+						arr.add(op[cnt++]);
+						
+					}
+				}
+
+			}
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 10; i++)
+				sb.append(arr.get(i) + " ");
+			System.out.printf("#%d %s\n", t, sb.toString());
+		}
+	}
+}

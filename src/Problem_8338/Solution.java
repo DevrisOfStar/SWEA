9120af993f8749131b381e3aea9175891d4ddbe8
nokda75 <yh1483@naver.com>
2019-08-16 10:05:16 +0900

    2019-08-16 Problem 8338 Solved.

--- src/Problem_8338/Solution.java
+++ src/Problem_8338/Solution.java
@@ -1,8 +1,10 @@
 package Problem_8338;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Solution {
 
@@ -26,7 +28,8 @@ public class Solution {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		String str;
 		for (int t = 1; t <= T; t++) {
@@ -37,8 +40,10 @@ public class Solution {
 				arr[i] = str.charAt(i*2)-'0';
 			}
 			
-		
-			System.out.printf("#%d %d\n",t, getmax());
+			sb.append("#").append(t).append(" ").append(getmax()).append("\n");
+			
 		}
+		bw.write(sb.toString().trim());
+		bw.flush();
 	}
 }

0d7e9d2a37440a0b90ee1663276348eaa2b9f1b9
yh1483 <yh1483@naver.com>
2019-09-16 17:14:48 +0900

    2019-09-16 Problem 8500 Solved.

---
+++ src/Problem_8500/Solution.java
@@ -0,0 +1,34 @@
+package Problem_8500;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import sun.util.locale.StringTokenIterator;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int N, cnt, a, max = Integer.MIN_VALUE;
+		StringTokenizer st;
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
+			cnt = 0;
+			st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 0; i < N; i++) {
+				a = Integer.parseInt(st.nextToken());
+				cnt += a;
+
+				if (max < a)
+					max = a;
+
+			}
+			cnt += max;
+			cnt += N;
+			System.out.printf("#%d %d\n", t, cnt);
+		}
+	}
+}
